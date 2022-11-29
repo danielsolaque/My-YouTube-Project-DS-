c96cfe484220e262ee94b8e909ba6d424bdd6b6a
SasukeDaTony <tonytaylor@pursuit.org>
2022-11-29 10:14:00 -0500

    fixed search modal url

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@ const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&
 
 // Error version for  Modal Presentation:
 
-//   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&q=${searchInput}&maxResults=10&channelId=-1`;
+//   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&maxResults=10&channelId=-1`;
 
 export function getYoutubeData(searchInput) {
   const request = fetch(BASE_URL.concat(`&q=${searchInput}`)).then((response) =>
