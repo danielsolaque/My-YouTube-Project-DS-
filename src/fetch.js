0fec45a3c329c8045a98af48192d05605561ff55
SasukeDaTony <tonytaylor@pursuit.org>
2022-11-29 18:19:53 -0500

    second Netlify Bug

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,8 @@
-// const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&maxResults=10`;
+const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&maxResults=10`;
 
 // Error version for  Modal Presentation:
 
-<<<<<<< HEAD
-const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&maxResults=10&channelId=-1`;
-=======
 //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&maxResults=10&channelId=-1`;
->>>>>>> modal-functionality-css
 
 export function getYoutubeData(searchInput) {
   const request = fetch(BASE_URL.concat(`&q=${searchInput}`)).then((response) =>
